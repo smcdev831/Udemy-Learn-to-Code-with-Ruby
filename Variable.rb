79c15c6bb7e064cf795797a0d3ec11a1ab957584
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-25 21:54:39 -0500

    Update Variable.rb

--- Variable.rb
+++ Variable.rb
@@ -2,3 +2,12 @@ name = "Boris"
 last_name = "Paskhaver"
 handsome = true
 age = 24 + 1
+
+puts name
+puts last_name
+puts name + last_name
+puts name + " " + last_name
+
+puts handsome
+
+puts age + 5
