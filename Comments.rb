c3ce251732f5183c36e0534b7c193e0f8d616354
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-25 21:02:56 -0500

    Update Comments.rb

--- Comments.rb
+++ Comments.rb
@@ -1,3 +1,26 @@
 # This is a comment
 
 # puts "Hello World"
+
+# This prints out "Hello World" to the Atom Runner output
+puts "Hello World"
+
+
+puts "Hello World" # Prints "Hello World" to the console
+
+#In this section of code
+# we are going to do operations
+# A, B, and C
+
+=begin
+puts 1 + 1
+puts 5 + 3
+puts "Hello World"
+p "Blah blah blah"
+=end
+
+# puts 1 + 1
+# puts 5 + 3
+# puts "Hello World"
+# p "Blah blah blah"
+#Highlight and ctrl+/
