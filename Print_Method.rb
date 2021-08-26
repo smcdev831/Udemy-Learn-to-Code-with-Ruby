03a6e8b28c1b1cdae99e770e8363b73f8e9e6fb5
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-25 20:09:52 -0500

    Update Print_Method.rb

--- Print_Method.rb
+++ Print_Method.rb
@@ -3,3 +3,20 @@ puts "World"
 
 print "Hello"
 print "World"
+print "blahblahblah"
+
+puts
+
+print "Hello"
+print " World"
+
+puts
+
+print "Hello "
+print "World"
+
+puts
+
+print "Hello"
+print " "
+print "World"
