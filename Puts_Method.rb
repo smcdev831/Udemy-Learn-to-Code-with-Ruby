e567a34fedd2fa958a7df02d65c4188d756c9a3f
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-25 20:05:39 -0500

    Update Puts_Method.rb

--- Puts_Method.rb
+++ Puts_Method.rb
@@ -8,3 +8,14 @@ puts "I made $65 dollars or a profit of 25%!"
 puts 5
 puts 3.14159
 puts "3.14159"
+
+puts 4+3
+puts "4+3"
+puts "4" + "3"
+
+
+puts
+puts
+puts
+
+puts "See, there's a bunch of line breaks above!"
