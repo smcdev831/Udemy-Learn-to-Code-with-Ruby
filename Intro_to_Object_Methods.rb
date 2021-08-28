8e2a93af36efd041214382dcb6d418945f6ae948
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-27 21:45:48 -0500

    Update Intro_to_Object_Methods.rb

---
+++ Intro_to_Object_Methods.rb
@@ -0,0 +1,24 @@
+p "hello world!"
+
+p "hello world!".length
+
+p "hello world!".upcase
+
+p "hello world!".downcase
+
+foo = "hello world!"
+
+p foo.length
+
+p foo.upcase
+
+p foo.downcase
+
+p 10
+
+p 10.next
+p -1.next
+
+p "Hello World"
+puts "Hello World"
+p "Hellow World".inspect
