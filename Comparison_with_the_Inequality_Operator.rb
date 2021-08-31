5bf37033354e468fd584af4269ab6563f1c5e322
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-30 21:10:04 -0500

    Create Comparison_with_the_Inequality_Operator.rb

---
+++ Comparison_with_the_Inequality_Operator.rb
@@ -0,0 +1,19 @@
+puts 10 != 5
+
+puts 10 != 10
+
+puts "Hello" != "Goodbye"
+
+puts "Hello" != "Hello"
+
+puts "Hello" != "hello"
+
+puts "Hello".downcase != "heLlo".downcase
+
+puts '123' != 123
+puts 123 != 123
+puts '123' != '123'
+
+puts 123.class
+puts 123.0.class
+puts '123'.class
