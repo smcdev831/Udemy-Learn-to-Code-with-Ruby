0dafc94543ecec269f333e522cca333fa3127c26
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-30 19:04:00 -0500

    Update String_Interpolation.rb

--- String_Interpolation.rb
+++ String_Interpolation.rb
@@ -6,3 +6,12 @@ name = "Boris"
 
 puts "Hello name, how are you?"
 puts "Hello #{name}, how are you?"
+
+age = 25
+
+puts "I am " + age.to_s + " years old"
+puts "I am #{age} years old"
+
+puts "The result of adding 1 + 1 is #{1 + 1}"
+
+puts "In 5 years, I will be #{age + 5} years old."
