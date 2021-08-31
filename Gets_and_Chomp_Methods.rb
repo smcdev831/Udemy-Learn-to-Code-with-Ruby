39ad0dfd59ecf3f13b1a6e865bf566ab110f763c
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-30 19:20:40 -0500

    Create Gets_and_Chomp_Methods.rb

---
+++ Gets_and_Chomp_Methods.rb
@@ -0,0 +1,14 @@
+# puts "What is your name?"
+#
+# name = gets.chomp
+#
+# puts "Thank you for giving me you name #{name}! See you later!"
+
+
+puts "Hi, what's your name?"
+name = gets.chomp
+
+puts "Great, what's your age?"
+age = gets.chomp.to_i
+
+puts "Cool, so your name is #{name} and you are #{age} years old."
