31af9d60b3feaeed1dcb4b5962acf2d366de9988
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-30 19:04:47 -0500

    Update String_Interpolation.rb

--- String_Interpolation.rb
+++ String_Interpolation.rb
@@ -15,3 +15,8 @@ puts "I am #{age} years old"
 puts "The result of adding 1 + 1 is #{1 + 1}"
 
 puts "In 5 years, I will be #{age + 5} years old."
+
+x = 5
+y = 8
+
+puts "The sum of x and y is #{x + y}"
