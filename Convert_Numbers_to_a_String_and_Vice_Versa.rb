687a4728bfce11b2a9ed8969067d5340e1dc23e0
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-30 20:40:52 -0500

    Update Convert_Numbers_to_a_String_and_Vice_Versa.rb

---
+++ Convert_Numbers_to_a_String_and_Vice_Versa.rb
@@ -0,0 +1,22 @@
+str = "5"
+puts str
+puts str.class
+
+puts str.to_i
+puts str.to_i.class
+
+puts str.to_f
+puts str.to_f.class
+
+number = 10
+puts number
+puts number.class
+puts number.to_s.class
+puts number.to_f.class
+
+pi = 3.14
+puts pi
+puts pi.class
+puts pi.to_i #this chops off the decimal, does not round
+puts pi.to_i.class
+puts pi.to_s.class
