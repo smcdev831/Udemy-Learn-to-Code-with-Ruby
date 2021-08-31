5d65227037a960971ba12da64157c39401049d11
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-30 20:29:18 -0500

    Create Intro_to_Numbers.rb

---
+++ Intro_to_Numbers.rb
@@ -0,0 +1,25 @@
+puts 5
+puts 5.next
+puts 5.class
+
+puts 0
+puts 0.next
+puts 0.class
+
+puts 100
+puts 100.class
+
+puts -837
+puts -837.class
+
+puts 5.987
+puts 5.987.class
+
+puts 2.71
+puts 2.71.class
+
+puts -10.23
+puts -10.23.class
+
+puts 999999999999999999999999999999999999999999999999999999
+puts 999999999999999999999999999999999999999999999999999999.class
