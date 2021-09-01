316b491aac9c349e5618a897b6005c651b91f2be
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-31 21:46:14 -0500

    Create Less_than_and_Greater_than_Operators.rb

---
+++ Less_than_and_Greater_than_Operators.rb
@@ -0,0 +1,19 @@
+puts 1 < 8
+puts 10 < 15
+puts 12 < 5
+
+puts 1 <= 5
+puts 13 <= 5
+puts 5 <= 5
+
+
+puts 10 > 5
+puts -1 > -5
+puts 12 > 20
+
+puts 12.83 < 21.14
+puts 12.83 > 21.14
+
+puts 8 >= 5
+puts 8 >= 10
+puts 8 >= 8
