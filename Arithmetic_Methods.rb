f7a2c678ccaeaa1b47e4abcea61c1824f78e28d3
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-09-14 15:57:47 -0500

    Update Arithmetic_Methods.rb

--- Arithmetic_Methods.rb
+++ Arithmetic_Methods.rb
@@ -1,8 +1,6 @@
 puts 1.next()
 
 puts 1 + 2
-puts 1.+(2)
-
 puts 10 - 5
 puts 3 * 4
 puts 10 / 5
@@ -10,3 +8,7 @@ puts 10 / 5
 puts 12 / 5
 puts 12.0 / 5.0
 puts 10 % 3
+
+#call with the .method and pass an arguement
+puts 1.+(2)
+puts 10.div(5)
