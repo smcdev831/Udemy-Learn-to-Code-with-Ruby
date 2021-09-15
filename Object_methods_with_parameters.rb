45daf2af1d034a85bca9b17acb9ecae7200e89e9
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-09-15 16:08:17 -0500

    Update Object_methods_with_parameters.rb

--- Object_methods_with_parameters.rb
+++ Object_methods_with_parameters.rb
@@ -5,3 +5,10 @@ puts 'cat'.between?('ant', 'dog')
 puts 'gnu'.between?('ant', 'dog')
 
 puts 20.between?(10, 30)
+puts 20.between?(30, 50)
+
+puts 1.2.between?(1, 3)
+puts 1.2.between?(1.1, 1.3)
+
+puts -10.5.between?(-20, 0)
+puts 5.3.between?(8.8, 9.6)
