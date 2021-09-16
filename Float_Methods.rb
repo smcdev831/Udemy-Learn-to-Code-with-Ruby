9ce30d242d977e4a68741976b20206281a00712e
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-09-16 16:12:07 -0500

    Update Float_Methods.rb

--- Float_Methods.rb
+++ Float_Methods.rb
@@ -3,3 +3,5 @@ puts 10.5.to_i #chops off the decimal, does not round
 puts 10.5.floor #rounds the number down to the lowest integer
 
 puts 10.5.ceil #rounds the number up to the next integer
+
+puts 3.14159.round
