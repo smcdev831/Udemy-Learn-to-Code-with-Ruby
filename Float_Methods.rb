f4ecf50de1eb320d5721581e71dc3f70b306b0b2
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-09-16 16:23:21 -0500

    Update Float_Methods.rb

--- Float_Methods.rb
+++ Float_Methods.rb
@@ -5,3 +5,7 @@ puts 10.5.floor #rounds the number down to the lowest integer
 puts 10.5.ceil #rounds the number up to the next integer
 
 puts 3.14159.round
+puts 3.6.round
+puts 3.14159.round(3)
+
+puts 3.14159.abs
