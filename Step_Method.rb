387191190889507eaecfbf75b3ed2737dc81b259
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-09-18 18:04:38 -0500

    Create Step_Method.rb

---
+++ Step_Method.rb
@@ -0,0 +1,13 @@
+1.step(100, 5) { |i| puts i }
+
+0.step(100, 5) { |i| puts i }
+
+
+0.step(85, 7) do |n|
+  puts "Alright, let's go up by 7 again!"
+  puts " I'm now on #{n}"
+end
+
+
+0.upto(10) { |n| puts n }
+0.step(10, 1) { |n| puts n }
