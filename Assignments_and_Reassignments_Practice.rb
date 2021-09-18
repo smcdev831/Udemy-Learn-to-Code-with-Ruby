0351c7d2dd18a1c68dc9aa436e39f7ce0a5201b0
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-09-18 17:13:18 -0500

    Create Assignments_and_Reassignments_Practice.rb

---
+++ Assignments_and_Reassignments_Practice.rb
@@ -0,0 +1,32 @@
+a = 15
+puts a
+
+a = 15
+a = a / 3
+puts a
+
+a = 15
+a /= 3
+puts a
+
+b = 23
+puts b
+
+b = 23
+b = b % 8
+puts b
+
+b = 23
+b %= 8
+puts b
+
+c = 10
+puts c
+
+c = 10
+c = c ** 5
+puts c
+
+c = 10
+c **= 5
+puts c
