cf33ef1b7ceffb4b7a814602a3ffe80514fc5cbe
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-09-18 17:18:51 -0500

    Update Blocks_With_Times_Method.rb

--- Blocks_With_Times_Method.rb
+++ Blocks_With_Times_Method.rb
@@ -2,3 +2,9 @@
   puts 'Boris is incredible'
   puts 'I am having so much fun learning Ruby'
 end
+
+3.times do |count|
+  puts "We are currently on loop number #{count}"
+  puts 'Boris is incredible'
+  puts 'I am having so much fun learning Ruby'
+end
