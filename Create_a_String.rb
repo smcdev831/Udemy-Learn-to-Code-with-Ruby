cb401e4df53de4a1555d492a0bcb9004ce12369d
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-09-19 21:21:37 -0500

    Update Create_a_String.rb

--- Create_a_String.rb
+++ Create_a_String.rb
@@ -4,3 +4,17 @@ puts "I can also include characters and numbers: # $ @ 5 9"
 name = "Boris"
 revenue = "$23 dollars"
 puts name, revenue
+
+space = " "
+puts space.length
+
+empty = ""
+puts empty.length
+
+puts name.class
+puts space.class
+puts revenue.class
+puts empty.class
+
+name = String.new("Boris")
+puts name
