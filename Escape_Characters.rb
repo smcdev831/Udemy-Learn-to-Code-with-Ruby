4bf1380de8ec590bcc525dacd85d83a9935bd643
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-09-20 22:08:48 -0500

    Update Escape_Characters.rb

--- Escape_Characters.rb
+++ Escape_Characters.rb
@@ -1,7 +1,22 @@
 puts "Hello world"
 puts 'Hello world'
 
+#different quotes
 puts "Juliet said 'Goodbye' to Romeo"
 puts 'Juliet said "Goodbye" to Romeo'
 
+#same quotes
 puts "Juliet said \"Goodbye\" to Romeo"
+puts 'Juliet said \'Goodbye\' to Romeo'
+
+#linebreak - it will be literal so if you have a space it will show in the output
+puts "Let's add a line break \n right here"
+puts "Let's add a line break\nright here"
+
+result = "Let's add a line break\nright here"
+puts result
+p result
+
+#add a tab
+puts "\tInsert a tab right here"
+puts "This is normal"
