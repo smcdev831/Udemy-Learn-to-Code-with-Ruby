8e6336a9f486638ddb6cc0efe578d4881eaa1fd7
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-09-21 20:14:50 -0500

    Create Concatenate_Strings.rb

---
+++ Concatenate_Strings.rb
@@ -0,0 +1,27 @@
+first_name = "Harry"
+last_name = "Potter"
+
+puts first_name + last_name
+puts first_name + " " + last_name
+
+full_name = first_name + last_name
+puts full_name
+
+puts first_name.concat(last_name)
+
+first_name = "Harry"
+last_name = "Potter"
+
+real_name = first_name << last_name << "wizard"
+puts real_name
+
+first_name = "Harry"
+last_name = "Potter"
+
+other_real_name = first_name + last_name + "wizard"
+puts other_real_name
+
+first_name = "Harry"
+last_name = "Potter"
+
+puts first_name.prepend(last_name)
