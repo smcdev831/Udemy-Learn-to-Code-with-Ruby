bedcf088efa8353c0e195d26fedb000e740c7a81
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-09-21 19:40:01 -0500

    Create Equality.rb

---
+++ Equality.rb
@@ -0,0 +1,22 @@
+a = "Hello"
+b = "hello"
+c = "Hello "
+
+puts a != "lion"
+
+puts a == b
+puts a == c
+
+a = "Hello"
+b = "hello"
+c = "Hello"
+
+puts a != b
+puts a != c
+
+puts "Apple" < "Banana"
+puts "Hello" < "Help"
+
+#uppercase is priority over lowercase A-Z, then a-z
+puts "A" < "a"
+puts "Z" < "a"
