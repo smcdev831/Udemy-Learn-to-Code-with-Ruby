7f005cf1792882736218eaa424e138a50deffce9
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-09-30 20:59:11 -0500

    Update Length_and_Size_Methods.rb

---
+++ Length_and_Size_Methods.rb
@@ -0,0 +1,19 @@
+a = "Hello world"
+b = "Hi, my name is Boris"
+c = " "
+d = ""
+
+puts a.length
+puts b.length
+puts c.length
+puts d.length
+
+puts a.size
+puts b.size
+puts c.size
+puts d.size
+
+puts a.length.class
+puts a.length.next
+puts a.length.odd?
+puts a.length.even?
