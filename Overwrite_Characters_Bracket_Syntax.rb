65c4796447a7dcb9dacf931ee38e3775e8ea002f
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-12 22:01:48 -0500

    Create Overwrite_Characters_Bracket_Syntax.rb

---
+++ Overwrite_Characters_Bracket_Syntax.rb
@@ -0,0 +1,15 @@
+thing = "rocket ship"
+puts thing[0]
+
+thing = "rocket ship"
+thing[0] = "p"
+puts thing
+
+fact = "I love blueberry pie"
+puts fact[7, 4]
+fact[7, 4] = "rasp"
+puts fact
+fact[7..10] = "blue"
+puts fact
+fact[2..5] = "absolutely adore"
+puts fact
