4bd0b5b35fe4bd76eeb7aeec50da206064ef5006
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-13 21:25:15 -0500

    Update Case_Method.rb

---
+++ Case_Method.rb
@@ -0,0 +1,18 @@
+puts "hello"
+puts "hello".capitalize
+puts "heLLo".capitalize
+puts "Hello".capitalize
+
+puts "hello12".capitalize
+puts "hello!@".capitalize
+puts "hello 56".capitalize
+puts "hello world".capitalize
+puts "hello World".capitalize
+
+puts "boris123".upcase
+puts "blah blah blah".upcase
+
+lowercase = "I'm patient"
+uppercase = lowercase.upcase
+puts lowercase
+puts uppercase
