967d81e8f6631c916ef68f0bd7fdefe850811d63
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-14 21:48:39 -0500

    Update Bang_Method.rb

--- Bang_Method.rb
+++ Bang_Method.rb
@@ -5,3 +5,15 @@ puts word
 word = "hello"
 puts word.capitalize!
 puts word
+
+puts word.upcase!
+puts word
+
+puts word.downcase!
+puts word
+
+puts word.reverse!
+puts word
+
+puts word.swapcase!
+puts word
