1dadc800f3423d7fcb18c3b512e77f9d51af5c62
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-14 22:07:17 -0500

    Create Include_Method.rb

---
+++ Include_Method.rb
@@ -0,0 +1,15 @@
+name = "Snow White"
+puts name.include?("S")
+puts name.include?("h")
+puts name.include?("s")
+puts name.include?(" ")
+puts name.include?("  ")
+
+puts name.downcase.include?("s")
+puts name.upcase.include?("OW")
+puts name
+
+puts name.downcase.include?("snow")
+puts name.downcase.include?("rain")
+puts name.downcase.include?("it")
+puts name.downcase.include?("sit")
