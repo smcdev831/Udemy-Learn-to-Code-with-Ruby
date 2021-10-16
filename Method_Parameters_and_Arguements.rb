dfadedeb8ce0a8c07034bc81168fd0cb9d317e2c
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-16 10:42:41 -0500

    Update Method_Parameters_and_Arguements.rb

--- Method_Parameters_and_Arguements.rb
+++ Method_Parameters_and_Arguements.rb
@@ -8,3 +8,17 @@ praise_person "Boris"
 praise_person "Dave"
 praise_person("Pizza")
 praise_person("SuSu")
+
+
+def praise_person(name, age)
+  puts "#{name} is amazing"
+  puts "#{name} is charming"
+  puts "#{name} is talented"
+  puts "Their age is #{age}"
+  puts "Their age in 5 years will be #{age + 5}"
+end
+
+praise_person "Boris", 25
+praise_person "Dave", 32
+praise_person("Pizza", 3)
+praise_person("SuSu", 40)
