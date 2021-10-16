76d4b68b5e4e75f60bf77129e10f13b78e7cf9e5
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-16 13:45:03 -0500

    Update If_Else_Method.rb

--- If_Else_Method.rb
+++ If_Else_Method.rb
@@ -7,3 +7,16 @@ elsif grade == "B"
 else
   puts "Unacceptable!"
 end
+
+def odd_or_even(number)
+  if number.odd?
+    "That number is odd"
+  else
+    "That number is even"
+  end
+end
+
+puts odd_or_even(5)
+puts odd_or_even(98)
+puts odd_or_even(11)
+puts odd_or_even(18561)
