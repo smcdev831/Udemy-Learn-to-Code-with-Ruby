bb69145e050874571218f640f132e4602f327045
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-16 10:33:32 -0500

    Create Local_variables.rb

---
+++ Local_variables.rb
@@ -0,0 +1,20 @@
+ def introduce_myself
+   expression = "I am a genius"
+   puts expression
+ end
+
+ introduce_myself
+ #puts expression - this is only available within the method
+
+introduce_myself
+introduce_myself
+introduce_myself
+
+expression = "I am handsome"
+def introduce_myself
+  expression = "I am a genius"
+  puts expression
+end
+
+introduce_myself
+puts expression
