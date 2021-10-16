db561dd7ebe590f4533794c90602b6d391d37f82
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-16 13:40:34 -0500

    Create If_Else_Method.rb

---
+++ If_Else_Method.rb
@@ -0,0 +1,9 @@
+grade = "C"
+
+if grade == "A"
+  puts "That's an excellent grade. Good job!"
+elsif grade == "B"
+  puts "That's a good grade. Let's bring it up next time!"
+else
+  puts "Unacceptable!"
+end
