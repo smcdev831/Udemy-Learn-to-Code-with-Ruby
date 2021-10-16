33a0380bfa1ede70d0b6a7dce974741547436fb8
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-16 13:28:00 -0500

    Create Truthiness_and_Fasliness.rb

---
+++ Truthiness_and_Fasliness.rb
@@ -0,0 +1,23 @@
+if 5 < 8
+  puts "That is true"
+end
+
+if true
+  puts "That is true"
+end
+
+if ""
+  puts "That is true"
+end
+
+if []
+  puts "That is true"
+end
+
+if false
+  puts "That is true"
+end
+
+if nil
+  puts "That is true"
+end
