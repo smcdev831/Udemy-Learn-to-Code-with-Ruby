cd6c272c90e8653dba776202aec5ccc27e2e8143
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-16 10:39:15 -0500

    Create Method_Parameters_and_Arguements.rb

---
+++ Method_Parameters_and_Arguements.rb
@@ -0,0 +1,10 @@
+def praise_person(name)
+  puts "#{name} is amazing"
+  puts "#{name} is charming"
+  puts "#{name} is talented"
+end
+
+praise_person "Boris"
+praise_person "Dave"
+praise_person("Pizza")
+praise_person("SuSu")
