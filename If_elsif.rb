d0580ca3fdb5d8a59386ab7100120a0b173519ea
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-16 13:38:44 -0500

    Create If_elsif.rb

---
+++ If_elsif.rb
@@ -0,0 +1,24 @@
+color = "Green"
+
+if color == "Red"
+  puts "Red is rad"
+end
+
+if color == "Green"
+  puts "Green is great!"
+end
+
+if color == "Red"
+  puts "Red is rad"
+elsif color == "Green"
+  puts "Green is great!"
+end
+
+number = 55
+if number < 25
+  puts "That's a low number!"
+elsif number < 50
+  puts "That's a number in the middle!"
+elsif number < 75
+  puts "That's a big number"
+end
