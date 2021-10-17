229d94887522e9facb621b9e341f4d94fba2bb9e
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-17 10:39:07 -0500

    Update Parentheses_Precedence.rb

--- Parentheses_Precedence.rb
+++ Parentheses_Precedence.rb
@@ -2,6 +2,10 @@ def authenticate_agent(rank, name, creditentials)
   if (rank == "007" && name == "James Bond") || creditentials == "Secret Agent"
     puts "Access granted, please proceed to Intelligence Department"
   else
-    puts "Access denied,. #{name}"
+    puts "Access denied, #{name}"
   end
 end
+
+authenticate_agent("007", "James Bond", "Spy")
+authenticate_agent("007", "Jim Bond", "Spy")
+authenticate_agent("007", "Jim Bond", "Secret Agent")
