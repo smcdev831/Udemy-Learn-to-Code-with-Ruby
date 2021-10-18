96928a48a6c093b462eb50f9d8d304a5e23e0783
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-18 10:45:02 -0500

    Update Nested_If_Statements.rb

--- Nested_If_Statements.rb
+++ Nested_If_Statements.rb
@@ -1,5 +1,11 @@
 def meal_plan(time_of_week, time_of_day)
   if time_of_week == "weekday"
+    if time_of_day == "breakfast"
+      puts "cereal"
+    elsif time_of_day == "lunch"
+      puts "sandwich"
+    elsif time_of_day == "dinner"
+      puts "chicken nuggets"      
   elsif time_of_week =="weekend"
-  end    
+  end
 end
