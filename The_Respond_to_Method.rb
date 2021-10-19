b9f3c22bd93a340aa79b4b8b88f00eeb55f8a66e
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-19 11:01:20 -0500

    Update The_Respond_to_Method.rb

--- The_Respond_to_Method.rb
+++ The_Respond_to_Method.rb
@@ -6,3 +6,12 @@ puts num.respond_to?("odd?")
 puts num.respond_to?("even?")
 puts num.respond_to?("respond_to?")
 puts num.respond_to?("length")
+
+
+puts "Hello".respond_to?("length")
+puts "Hello".respond_to?("class")
+puts "Hello".respond_to?("upcase")
+puts "Hello".respond_to?("odd?")
+
+puts "Hello".respond_to?("length")
+puts "Hello".respond_to?(:length)
