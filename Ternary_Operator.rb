8ec183a72ecfcbd77eb21017f9b5938b8295cb87
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-20 11:12:52 -0500

    Update Ternary_Operator.rb

--- Ternary_Operator.rb
+++ Ternary_Operator.rb
@@ -5,3 +5,19 @@ else
 end
 
 puts 1 < 2 ? "Yes, it is" : "No, it's not"
+
+if "yes" == "yes"
+  puts "The two are equal"
+else
+  puts "The two are not equal"
+end
+
+puts "yes" == "yes" ? "The two are equal" : "The two are not equal"
+puts "yes" == "no" ? "The two are equal" : "The two are not equal"
+
+def even_or_odd(number)
+  number.even? ? "That number is even" : "That number is odd"
+end
+
+puts even_or_odd(6)
+puts even_or_odd(11)
