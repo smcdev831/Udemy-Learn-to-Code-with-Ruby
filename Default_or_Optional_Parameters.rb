84117580b31a3190abcb3c9237a5e510af3d5367
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-21 11:27:34 -0500

    Update Default_or_Optional_Parameters.rb

--- Default_or_Optional_Parameters.rb
+++ Default_or_Optional_Parameters.rb
@@ -1,2 +1,3 @@
-def make_phone_call(number, international_code, area_code )
+def make_phone_call(number, international_code = 1, area_code = 646)
+  puts "I am calling the number #{international_code} - #{area_code} - #{number}"
 end
