c81ef71772b2e35a8c9ad68fc7cc120bb9aabef9
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-25 17:40:51 -0500

    Update Case_Statements.rb

--- Case_Statements.rb
+++ Case_Statements.rb
@@ -1,5 +1,14 @@
 def rate_my_food(food)
   case food
-  when condition
+  when "Steak"
+    "Pass the steak sauce! That's delicious!"
+  when "Sushi"
+    "Great choice! My favorite food"
+  when "Tacos", "Burritos", "Quesadillas"
+    "Cheesy and filling! The perfect combination"
+  when "Tofu", "Brussels Sprouts"
+    "Disgusting! Yuck!"
+  else
+    "I don't know about that food"
   end
 end
