139fd32bf614d67f38f48a77864c08476a1775d8
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-26 19:02:02 -0500

    Update CHALLENGE_FizzBuzz.rb

--- CHALLENGE_FizzBuzz.rb
+++ CHALLENGE_FizzBuzz.rb
@@ -18,4 +18,256 @@ def fizzbuzz(number)
   end
 end
 
-fizzbuzz(25)
+fizzbuzz(250)
+
+# Atom Runner: CHALLENGE_FizzBuzz.rb
+# 1
+# 2
+# Fizz for 3
+# 4
+# Buzz for 5
+# Fizz for 6
+# 7
+# 8
+# Fizz for 9
+# Buzz for 10
+# 11
+# Fizz for 12
+# 13
+# 14
+# Fizzbuzz for 15
+# 16
+# 17
+# Fizz for 18
+# 19
+# Buzz for 20
+# Fizz for 21
+# 22
+# 23
+# Fizz for 24
+# Buzz for 25
+# 26
+# Fizz for 27
+# 28
+# 29
+# Fizzbuzz for 30
+# 31
+# 32
+# Fizz for 33
+# 34
+# Buzz for 35
+# Fizz for 36
+# 37
+# 38
+# Fizz for 39
+# Buzz for 40
+# 41
+# Fizz for 42
+# 43
+# 44
+# Fizzbuzz for 45
+# 46
+# 47
+# Fizz for 48
+# 49
+# Buzz for 50
+# Fizz for 51
+# 52
+# 53
+# Fizz for 54
+# Buzz for 55
+# 56
+# Fizz for 57
+# 58
+# 59
+# Fizzbuzz for 60
+# 61
+# 62
+# Fizz for 63
+# 64
+# Buzz for 65
+# Fizz for 66
+# 67
+# 68
+# Fizz for 69
+# Buzz for 70
+# 71
+# Fizz for 72
+# 73
+# 74
+# Fizzbuzz for 75
+# 76
+# 77
+# Fizz for 78
+# 79
+# Buzz for 80
+# Fizz for 81
+# 82
+# 83
+# Fizz for 84
+# Buzz for 85
+# 86
+# Fizz for 87
+# 88
+# 89
+# Fizzbuzz for 90
+# 91
+# 92
+# Fizz for 93
+# 94
+# Buzz for 95
+# Fizz for 96
+# 97
+# 98
+# Fizz for 99
+# Buzz for 100
+# 101
+# Fizz for 102
+# 103
+# 104
+# Fizzbuzz for 105
+# 106
+# 107
+# Fizz for 108
+# 109
+# Buzz for 110
+# Fizz for 111
+# 112
+# 113
+# Fizz for 114
+# Buzz for 115
+# 116
+# Fizz for 117
+# 118
+# 119
+# Fizzbuzz for 120
+# 121
+# 122
+# Fizz for 123
+# 124
+# Buzz for 125
+# Fizz for 126
+# 127
+# 128
+# Fizz for 129
+# Buzz for 130
+# 131
+# Fizz for 132
+# 133
+# 134
+# Fizzbuzz for 135
+# 136
+# 137
+# Fizz for 138
+# 139
+# Buzz for 140
+# Fizz for 141
+# 142
+# 143
+# Fizz for 144
+# Buzz for 145
+# 146
+# Fizz for 147
+# 148
+# 149
+# Fizzbuzz for 150
+# 151
+# 152
+# Fizz for 153
+# 154
+# Buzz for 155
+# Fizz for 156
+# 157
+# 158
+# Fizz for 159
+# Buzz for 160
+# 161
+# Fizz for 162
+# 163
+# 164
+# Fizzbuzz for 165
+# 166
+# 167
+# Fizz for 168
+# 169
+# Buzz for 170
+# Fizz for 171
+# 172
+# 173
+# Fizz for 174
+# Buzz for 175
+# 176
+# Fizz for 177
+# 178
+# 179
+# Fizzbuzz for 180
+# 181
+# 182
+# Fizz for 183
+# 184
+# Buzz for 185
+# Fizz for 186
+# 187
+# 188
+# Fizz for 189
+# Buzz for 190
+# 191
+# Fizz for 192
+# 193
+# 194
+# Fizzbuzz for 195
+# 196
+# 197
+# Fizz for 198
+# 199
+# Buzz for 200
+# Fizz for 201
+# 202
+# 203
+# Fizz for 204
+# Buzz for 205
+# 206
+# Fizz for 207
+# 208
+# 209
+# Fizzbuzz for 210
+# 211
+# 212
+# Fizz for 213
+# 214
+# Buzz for 215
+# Fizz for 216
+# 217
+# 218
+# Fizz for 219
+# Buzz for 220
+# 221
+# Fizz for 222
+# 223
+# 224
+# Fizzbuzz for 225
+# 226
+# 227
+# Fizz for 228
+# 229
+# Buzz for 230
+# Fizz for 231
+# 232
+# 233
+# Fizz for 234
+# Buzz for 235
+# 236
+# Fizz for 237
+# 238
+# 239
+# Fizzbuzz for 240
+# 241
+# 242
+# Fizz for 243
+# 244
+# Buzz for 245
+# Fizz for 246
+# 247
+# 248
+# Fizz for 249
+# Buzz for 250
