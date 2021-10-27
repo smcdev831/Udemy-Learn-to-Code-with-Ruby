1c021a2b2a2db86d64b3ab6e93fe7ca6520dd847
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-26 21:11:03 -0500

    Create Generate_a_Random_Number_with_the_Rand_Method.rb

---
+++ Generate_a_Random_Number_with_the_Rand_Method.rb
@@ -0,0 +1,11 @@
+puts rand #puts a number between 0 and 1
+
+puts rand.round(4)
+
+puts rand(100) #not inclusive of the arguement
+
+puts rand * 100
+
+puts rand(50..60)
+puts rand(50...60)
+puts rand(1..100)
