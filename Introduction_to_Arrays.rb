f393476b5aa9183c2f443980cf552da58db2bfc7
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-10-26 22:07:02 -0500

    Create Introduction_to_Arrays.rb

---
+++ Introduction_to_Arrays.rb
@@ -0,0 +1,27 @@
+puts []
+puts [].class
+
+numbers = [4, 8, 15, 16, 23, 42]
+p numbers
+puts numbers
+print numbers
+
+toys = ["Teddy Bear", "Super Soaker", "Board Game"]
+p toys
+puts toys
+print toys
+
+things = [4, true, "Hello", 10.99]
+p things
+puts things
+print things
+
+registrations = [true, true, false, true, false]
+p registrations
+puts registrations
+print registrations
+
+students = [["Boris", 25, true], ["Sally", 23, true], ["Ingrid", 31, false]]
+p students
+puts students
+print students
