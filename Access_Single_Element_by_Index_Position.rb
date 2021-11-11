ddde40a65566f9f872297eb3c6d765ed2b5c3434
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-11-11 13:05:20 -0600

    Create Access_Single_Element_by_Index_Position.rb

---
+++ Access_Single_Element_by_Index_Position.rb
@@ -0,0 +1,19 @@
+fruits = ["Apple", "Orange", "Grape", "Banana"]
+puts fruits.length
+
+puts fruits[0]
+puts fruits[1]
+puts fruits[3]
+
+p fruits[10]
+p fruits[100]
+p fruits[5]
+
+fruits = ["Apple", "Orange", "Grape", "Banana"]
+puts fruits.length
+
+puts fruits[-1]
+puts fruits[3]
+puts fruits[-3]
+puts fruits.[](3)
+puts fruits.[](-3)
