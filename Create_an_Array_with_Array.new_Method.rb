3d0e4ada554251152455987964eafc1206e2faa8
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-11-11 13:05:22 -0600

    Create Create_an_Array_with_Array.new_Method.rb

---
+++ Create_an_Array_with_Array.new_Method.rb
@@ -0,0 +1,11 @@
+puts [1, 2, 3].class
+puts ["Hello", "World"].class
+
+p Array.new(1)
+p Array.new(3)
+p Array.new(5)
+
+p Array.new(10, nil)
+p Array.new(10, true)
+p Array.new(10, "Hello")
+p Array.new(10, [1, 2, 3])
