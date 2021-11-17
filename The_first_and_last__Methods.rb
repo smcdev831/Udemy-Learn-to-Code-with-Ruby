e8ada7fce2145cade7524566f3cd0fd531c4c8c4
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-11-16 19:49:44 -0600

    Update The_first_and_last__Methods.rb

--- The_first_and_last__Methods.rb
+++ The_first_and_last__Methods.rb
@@ -8,3 +8,30 @@ puts arr.last(2)
 
 puts arr.first(1)
 puts arr.last(1)
+
+
+#if you pass an argument, it will return an array
+puts arr.first.class
+puts arr.last.class
+
+puts arr.first(1).class
+puts arr.last(1).class
+
+arr = [1, 3, 5, 7, 9, 15, 21, 18, 6]
+def custom_first(arr, num = 0)
+  return arr[0] if num == 0
+  arr[0, num]
+end
+
+puts custom_first(arr)
+puts custom_first(arr, 5)
+puts custom_first(arr, 1)
+
+def custom_last(arr, num = 0)
+  return arr[-1] if num == 0
+  arr[-num..-1]
+end
+
+puts custom_last(arr)
+puts custom_last(arr, 5)
+puts custom_last(arr, 1)
