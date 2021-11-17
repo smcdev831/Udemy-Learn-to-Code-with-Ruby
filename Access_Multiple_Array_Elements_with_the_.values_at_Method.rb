b8c30265c4b155f58a01f435cd9de20172029fab
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-11-16 18:58:32 -0600

    Update Access_Multiple_Array_Elements_with_the_.values_at_Method.rb

--- Access_Multiple_Array_Elements_with_the_.values_at_Method.rb
+++ Access_Multiple_Array_Elements_with_the_.values_at_Method.rb
@@ -3,3 +3,6 @@ channels = ["CBS", "FOX", "NBC", "EPSN", "UPN"]
 puts channels.values_at(0)
 puts channels.values_at(4)
 puts channels.values_at(0, 2, 4)
+puts channels.values_at(3, 3, 4)
+puts channels.values_at(3, 3, 5, 2, 100)
+puts channels.values_at(3, 3, 5, 2, 100, -3)
