25bd0e4726f1c029a41a0e43fbac44ec43adb1b6
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-11-16 19:13:51 -0600

    Create The_Length_and_Size_Methods.rb

---
+++ The_Length_and_Size_Methods.rb
@@ -0,0 +1,8 @@
+p "Hello world".length
+p "Hello world".size
+
+puts [1, 2, 3, 4].length
+puts [1, 2, 3, 4].size
+
+puts [1, nil, 2, 3, nil, 4, "Hello", 3.14].length
+puts [1, nil, 2, 3, nil, 4, "Hello", 3.14].size
