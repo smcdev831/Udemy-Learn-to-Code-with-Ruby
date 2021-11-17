f0ba0b818f3bdab8b9f8f58869d90fec957d94c8
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-11-16 19:05:48 -0600

    Create Overwrite_One_or_More_Array_Elements.rb

---
+++ Overwrite_One_or_More_Array_Elements.rb
@@ -0,0 +1,31 @@
+fruits = ["Apple", "Orange", "Grape", "Banana"]
+puts fruits
+
+fruits[1] = "Watermelon"
+puts fruits
+
+fruits[-1] = "Bananas"
+puts fruits
+
+puts fruits[4].class
+fruits[4] = "Raspberry"
+puts fruits
+
+fruits[5] = "Strawberry"
+puts fruits
+
+fruits[10] = "Kiwi"
+p fruits
+#Apple", "Watermelon", "Grape", "Bananas", "Raspberry", "Strawberry", nil, nil, nil, nil, "Kiwi"]
+puts fruits
+# Apple
+# Watermelon
+# Grape
+# Bananas
+# Raspberry
+# Strawberry
+#
+#
+#
+#
+# Kiwi
