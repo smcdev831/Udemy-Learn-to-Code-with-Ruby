7d1fb631f2d725cc0aea79bbb011398a63313360
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-11-16 19:16:55 -0600

    Update The_Length_and_Size_Methods.rb

--- The_Length_and_Size_Methods.rb
+++ The_Length_and_Size_Methods.rb
@@ -6,3 +6,15 @@ puts [1, 2, 3, 4].size
 
 puts [1, nil, 2, 3, nil, 4, "Hello", 3.14].length
 puts [1, nil, 2, 3, nil, 4, "Hello", 3.14].size
+
+p [].length
+p [].size
+
+p [1, 2, 3, 4, 2, 3, 2, nil].count
+p [1, 2, 3, 4, 2, 3, 2, nil].count(2)
+
+p [true, true, false, false, false, false, true].count(true)
+p [true, true, false, false, false, false, true].count(false)
+p [true, true, false, false, false, false, true].count("false")
+p [true, true, false, false, false, false, true].count(nil)
+p [true, true, false, nil, false, false, false, true].count(nil)
