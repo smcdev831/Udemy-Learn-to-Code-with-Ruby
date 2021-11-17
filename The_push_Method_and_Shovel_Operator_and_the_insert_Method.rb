0002f6a973cdc32a658e6a491359b74139432a42
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-11-16 20:03:04 -0600

    Create The_push_Method_and_Shovel_Operator_and_the_insert_Method.rb

---
+++ The_push_Method_and_Shovel_Operator_and_the_insert_Method.rb
@@ -0,0 +1,8 @@
+locations = ["House", "Airport", "Bar"]
+puts locations
+locations.push("Restaurant")
+puts locations
+
+locations = ["House", "Airport", "Bar"]
+locations.push("Restaurant", "Saloon")
+puts locations
