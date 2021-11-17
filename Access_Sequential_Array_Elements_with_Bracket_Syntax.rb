17daf1f3e7b1f314e97d65eb2c20e4757d19d460
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-11-16 18:49:53 -0600

    Update Access_Sequential_Array_Elements_with_Bracket_Syntax.rb

--- Access_Sequential_Array_Elements_with_Bracket_Syntax.rb
+++ Access_Sequential_Array_Elements_with_Bracket_Syntax.rb
@@ -2,3 +2,12 @@ numbers = [1, 3, 5, 7, 9, 15, 21]
 puts numbers
 puts numbers[2]
 puts numbers[2, 4]
+puts numbers[1, 3]
+
+puts numbers[0].class
+puts numbers[0, 1].class
+
+puts numbers[4, 100]
+puts numbers[4]
+puts numbers[100]
+p numbers[4, 100]
