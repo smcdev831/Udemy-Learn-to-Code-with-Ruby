23220a714e54de55136243655ab37e13495f85f9
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-12-27 13:57:48 -0600

    Update Shift_and_unshift_Method.rb

---
+++ Shift_and_unshift_Method.rb
@@ -0,0 +1,22 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+p arr
+extract = arr.shift
+
+p arr
+p extract
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+p arr
+extract = arr.shift(1)
+
+p arr
+p extract
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+p arr
+extract = arr.unshift(25)
+
+p arr
