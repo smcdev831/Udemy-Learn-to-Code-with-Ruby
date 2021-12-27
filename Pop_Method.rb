c63268f515f0abc2dd22befd9d7203b2432f6a3c
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-12-27 13:44:17 -0600

    Create Pop_Method.rb

---
+++ Pop_Method.rb
@@ -0,0 +1,26 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p arr
+arr.pop
+p arr
+arr.pop
+p arr
+last_item = arr.pop
+p arr
+p last_item
+
+two_items = arr.pop(2)
+p arr
+p two_items
+
+five_items = arr.pop(5)
+p arr
+p five_items
+
+#if you pass an arguement it will be an array
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+item = arr.pop
+p item
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+item = arr.pop(1)
+p item
