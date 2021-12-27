c6b2f1a4dfbfe4cff2f20610995baf58bfbe8cad
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-12-27 14:36:08 -0600

    Create Convert_Range_to_Array.rb

---
+++ Convert_Range_to_Array.rb
@@ -0,0 +1,16 @@
+letters_range = "A".."T"
+p letters_range
+letters_array = letters_range.to_a
+p letters_array[0]
+p letters_array[10]
+p letters_array[5]
+
+numbers_range = 415..450
+p numbers_range
+numbers_array = numbers_range.to_a
+p numbers_array
+p numbers_array.length
+p numbers_array.size
+p numbers_array[0]
+p numbers_array[-1]
+p numbers_array[10..15]
